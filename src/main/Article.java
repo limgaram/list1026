bb4f75b1f6543acf11cd2e3bde0aee5b86664770
unknown <limgaram>
2020-10-26 10:53:48 +0900

    list1026

---
+++ src/main/Article.java
@@ -0,0 +1,72 @@
+package main;
+
+public class Article {
+
+	private int id;
+	private String title;
+	private String body;
+	private String regDate;
+	private String nickname;
+	private int hit;
+
+	public Article() {
+
+	}
+
+	public Article(int id, String title, String body, String nickname, String regDate) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
+		this.regDate = regDate;
+		this.nickname = nickname;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	public String getRegDate() {
+		return regDate;
+	}
+
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
+
+	public int getHit() {
+		return hit;
+	}
+
+	public void setHit(int hit) {
+		this.hit = hit;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+}
